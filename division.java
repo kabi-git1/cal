694b7577ed7e004bdee9809b0694a4b46c9c8be6
tilak bahadur sunuwar <tilakbahadursunuwar03@gmail.com>
2024-05-20 04:13:50 +0545

    division work is complete

--- division.java
+++ division.java
@@ -3,7 +3,7 @@ class division
 {
     public static void main(String[]args)
     {
-       Scanner ob=new Scanner(System.in);
+      Scanner ob=new Scanner(System.in);
       int a= ob.nextInt();
       int b=ob.nextInt();
       int div=a/b;
