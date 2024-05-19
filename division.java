6afd7611977dd9781f925ccc1e6e1029bce2f87d
tilak bahadur sunuwar <tilakbahadursunuwar03@gmail.com>
2024-05-19 20:00:00 +0545

    division is complete

---
+++ division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class division
+{
+    public static void main(String[]args)
+    {
+       Scanner ob=new Scanner(System.in);
+      int a= ob.nextInt();
+      int b=ob.nextInt();
+      int div=a/b;
+      System.out.print("division of a & b is:"+div);
+    }
+}
