8e775fb5917fa7a0b9603fd4664062fad9f34cf6
tilak bahadur sunuwar <tilakbahadursunuwar03@gmail.com>
2024-05-19 19:43:43 +0545

    multiplication is complete

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class multiplication
+{
+    public static void main(String[]args)
+    {
+       Scanner ob=new Scanner(System.in);
+      int a= ob.nextInt();
+      int b=ob.nextInt();
+      int mul=a*b;
+      System.out.print("multiplication of a & b is:"+mul);
+    }
+}
